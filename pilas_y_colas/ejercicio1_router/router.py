f866ec57b9d60bce87208cf5b47a07df4a6ecba9
ERICKJLLO <erickjllo07@gmail.com>
2026-09-23 19:34:07 -0500

    Agregar método addPacket para gestionar la cola de paquetes en el Router

--- pilas_y_colas/ejercicio1_router/router.py
+++ pilas_y_colas/ejercicio1_router/router.py
@@ -7,3 +7,28 @@ class Router:
     def __init__(self, memoryLimit):
         self.memoryLimit = memoryLimit
         self.paquetes = Queue()
+        
+    def addPacket(self, source, destination, timestamp):
+        cantidad = self.paquetes.len()
+
+        for i in range(cantidad):
+            paquete = self.paquetes.dequeue()
+
+            if paquete.source == source and paquete.destination == destination and paquete.timestamp == timestamp:
+                self.paquetes.enqueue(paquete)
+
+                for j in range(i + 1, cantidad):
+                    paquete = self.paquetes.dequeue()
+                    self.paquetes.enqueue(paquete)
+
+                return False
+
+            self.paquetes.enqueue(paquete)
+
+        if self.paquetes.len() == self.memoryLimit:
+            self.paquetes.dequeue()
+
+        nuevo_paquete = Paquete(source, destination, timestamp)
+        self.paquetes.enqueue(nuevo_paquete)
+
+        return True
