8d5a86be574bcc63fc2d3b0ce75d5bdf0abae017
ERICKJLLO <erickjllo07@gmail.com>
2026-09-23 19:26:12 -0500

    Agregar pruebas de escenarios para la función rotar_hasta_maximo y mejorar la estructura del código

--- listas_enlazadas/ejercicio4_rotar/main.py
+++ listas_enlazadas/ejercicio4_rotar/main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
 from dlinkedlist import dlinkedlist
 
 
@@ -41,3 +46,43 @@ def mostrar_lista(lista):
         actual = actual.next
 
     print()
+
+
+print("ESCENARIO 1")
+
+lista = dlinkedlist()
+
+lista.append(10)
+lista.append(20)
+lista.append(30)
+lista.append(40)
+lista.append(50)
+
+print("Lista original:")
+mostrar_lista(lista)
+
+rotar_hasta_maximo(lista)
+
+print("Lista resultante:")
+mostrar_lista(lista)
+
+
+print()
+print("ESCENARIO 2")
+
+lista = dlinkedlist()
+
+lista.append(15)
+lista.append(8)
+lista.append(25)
+lista.append(12)
+lista.append(40)
+lista.append(6)
+
+print("Lista original:")
+mostrar_lista(lista)
+
+rotar_hasta_maximo(lista)
+
+print("Lista resultante:")
+mostrar_lista(lista)
