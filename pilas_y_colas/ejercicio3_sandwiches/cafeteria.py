70587d5b18c7d92d30870913c444aa777328865e
ERICKJLLO <erickjllo07@gmail.com>
2026-09-23 21:30:49 -0500

    Agregar clase Estudiante con atributos de preferencia y reintentos

---
+++ pilas_y_colas/ejercicio3_sandwiches/cafeteria.py
@@ -0,0 +1,11 @@
+from queue import Queue
+from stack import Stack
+
+
+class Estudiante:
+
+    def __init__(self, preferencia):
+        self.preferencia = preferencia
+        self.reintentos = 2
+
+
