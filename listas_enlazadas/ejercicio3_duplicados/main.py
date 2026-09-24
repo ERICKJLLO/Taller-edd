f1c2193d16c9b12e33f571644be9bb8ade90b5d6
ERICKJLLO <erickjllo07@gmail.com>
2026-09-23 19:19:26 -0500

    ejercicio3_duplicados: Agregar función eliminar_duplicados para eliminar elementos duplicados de la lista enlazada

---
+++ listas_enlazadas/ejercicio3_duplicados/main.py
@@ -0,0 +1,36 @@
+from dlinkedlist import dlinkedlist
+
+
+def eliminar_duplicados(lista):
+    actual = lista.head
+
+    while actual is not None:
+        siguiente = actual.next
+
+        while siguiente is not None:
+            siguiente_nodo = siguiente.next
+
+            if siguiente.value == actual.value:
+                anterior = siguiente.prev
+                posterior = siguiente.next
+
+                if anterior is not None:
+                    anterior.next = posterior
+
+                if posterior is not None:
+                    posterior.prev = anterior
+
+                if siguiente == lista.tail:
+                    lista.tail = anterior
+
+                lista.size -= 1
+
+            siguiente = siguiente_nodo
+
+        actual = actual.next
+
+    if lista.head is not None:
+        lista.head.prev = None
+
+    if lista.tail is not None:
+        lista.tail.next = None
